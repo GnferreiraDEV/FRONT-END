5ad8692192aab5a76ca965643c0c57f0b48a434d
GnferreiraDEV <165603002+GnferreiraDEV@users.noreply.github.com>
2024-11-14 15:10:19 -0300

    Update

---
+++ JavaScript/script/script.js
@@ -0,0 +1,11 @@
+// Caixa de texto:
+// confirm('Deseja baixar novamente?')
+// alert("Hello world!")
+
+var nome = prompt('What is your name?');
+document.write('Your name is: ' + nome + '<br>');
+
+
+var idade = prompt('How old are you?');
+document.write('Your have: ' + idade + ' years old.');
+
