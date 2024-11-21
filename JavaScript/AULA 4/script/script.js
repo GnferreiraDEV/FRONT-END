04e6d0dcfea2304c7733dc0d2572c60a26f3cd6d
Gustavo Ferreira <Gustavo.nferreira@a.ucb.br>
2024-11-21 17:07:10 -0300

    UPDATE

--- JavaScript/AULA 4/script/script.js
+++ JavaScript/AULA 4/script/script.js
@@ -1,3 +1,21 @@
 function testes(){
-    
+
+   
+} 
+
+function carregarCatalogo(){
+    for (let i = 0; i < 4; i++) { // A condição foi corrigida de 1 < 4 para i < 4
+        document.getElementById('catalogo').innerHTML += `
+        <div class="livro">
+            <img src="icons/image/livro1.jpg" alt="">
+            <h4>Harry Potter e a Pedra Filosofal</h4>
+
+            <button>
+                Adicionar
+                <img src="icons/plus.svg" alt="">
+            </button>
+        </div>
+        `;
+    }
 }
+carregarCatalogo();
