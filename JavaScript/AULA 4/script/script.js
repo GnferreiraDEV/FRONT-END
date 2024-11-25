49dc70348ff784475959a98b0aa4b31eb3519c21
Gustavo Ferreira <Gustavo.nferreira@a.ucb.br>
2024-11-25 14:29:34 -0300

    UPDATE

--- JavaScript/AULA 4/script/script.js
+++ JavaScript/AULA 4/script/script.js
@@ -4,14 +4,14 @@ function testes(){
 } 
 
 function carregarCatalogo(){
-    // Array de objetos contendo as informações dos livros
+    
     const livros = [
         { imagem: 'icons/image/livro1.jpg', titulo: 'Harry Potter e a Pedra Filosofal' },
         { imagem: 'icons/image/livro2.jpg', titulo: 'Harry Potter e a Câmara Secreta' },
         { imagem: 'icons/image/livro3.jpg', titulo: 'Harry Potter e o Prisioneiro de Azkaban' },
         { imagem: 'icons/image/livro4.jpg', titulo: 'Harry Potter e o Cálice de Fogo' },
-        { imagem: 'icons/image/livro5.jpg', titulo: 'Harry Potter e a Ordem da Fênix' },  // Novo livro 1
-        { imagem: 'icons/image/livro6.jpg', titulo: 'Harry Potter e o Enigma do Príncipe' },  // Novo livro 2
+        { imagem: 'icons/image/livro5.jpg', titulo: 'Harry Potter e a Ordem da Fênix' },  
+        { imagem: 'icons/image/livro6.jpg', titulo: 'Harry Potter e o Enigma do Príncipe' },  
        
     ];
 
@@ -57,6 +57,9 @@ function carregarFilmes() {
         `;
     }
 }
+
+
+
 carregarFilmes();  
 
 
