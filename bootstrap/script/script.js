111b7da19b6714827298d7989252f2996d158715
Gustavo Ferreira <Gustavo.nferreira@a.ucb.br>
2024-12-06 17:13:54 -0300

    UPDATE

--- bootstrap/script/script.js
+++ bootstrap/script/script.js
@@ -1,44 +1,140 @@
 lista_categorias = []
 
 class Categoria {
-    constructor(titulo,img,desc,marca){
+    constructor(titulo, img, desc, marca, ingredientes){
         this.titulo = titulo
         this.img = img
         this.desc = desc
         this.marca = marca
+        this.ingredientes = ingredientes
     }
 }
 
-lista_categorias.push(new Categoria('Jack & Coke','image/preto.jpg','mais diversos sucos! Laranja, morango e muito mais.','Suco feito por @Marca'))
-lista_categorias.push(new Categoria('Aperol Spritz','image/aperol.jpg','O Aperol Spritz é um drink de origem italiana que ficou conhecido em toda a Europa como a bebida do verão. Logo o Aperol Spritz chegou ao Brasil e não demorou para conquistar o nosso paladar. :)','refri feito por @Marca'))
-lista_categorias.push(new Categoria('Gin Tônica','image/gin.jpg','Refrigerante top :)','refri feito por @Marca'))
-lista_categorias.push(new Categoria('Dry Martini','image/dry.jpg','Refrigerante top :)','refri feito por @Marca'))
-lista_categorias.push(new Categoria('Marquerita','image/marguerita.jpg','Refrigerante top :)','refri feito por @Marca'))
-lista_categorias.push(new Categoria('Negroni','image/negroni.jpg','O Negroni é considerado um dos mais famosos coquetéis italianos no mundo. Inventado em 1919 pelo Conde Negroni, que pediu para adicionarem um toque de gin em vez de água com gás ao seu Americano.:)','refri feito por @Marca'))
-lista_categorias.push(new Categoria('Whisky','image/whisky.jpg','Refrigerante top :)','refri feito por @Marca'))
-lista_categorias.push(new Categoria('Moscow Mule','image/moscow.jpg','Refrigerante top :)','refri feito por @Marca'))
+lista_categorias.push(new Categoria(
+    'Jack & Coke',
+    'image/preto.jpg',
+    'Uma combinação clássica de whisky e refrigerante, perfeita para qualquer ocasião.',
+    '@HÉLEN',
+    [
+        '50ml de Jack Daniel\'s',
+        '150ml de Coca-Cola',
+        'Gelo'
+    ]
+));
+
+lista_categorias.push(new Categoria(
+    'Aperol Spritz',
+    'image/aperol.jpg',
+    'Um drink refrescante e leve, ideal para os dias quentes.',
+    '@FERREIRA',
+    [
+        '60ml de Aperol',
+        '90ml de Prosecco',
+        '30ml de Água com gás',
+        ,
+        'Fatias de laranja'
+    ]
+));
+
+lista_categorias.push(new Categoria(
+    'Gin Tônica',
+    'image/gin.jpg',
+    'Refrescante e elegante, o gin tônica é um clássico.',
+    '@DUDA',
+    [
+        '50ml de Gin',
+        '150ml de Água tônica',
+        'Fatias de limão',
+        'Gelo'
+    ]
+));
+
+lista_categorias.push(new Categoria(
+    'Dry Martini',
+    'image/dry.jpg',
+    'Um dos drinks mais sofisticados e famosos do mundo.',
+    '@RARYSSON',
+    [
+        '60ml de Gin',
+        '10ml de Vermute seco',
+        'Azeitona ou casca de limão para decorar'
+    ]
+));
+
+lista_categorias.push(new Categoria(
+    'Marquerita',
+    'image/marguerita.jpg',
+    'Tequila, limão e sal: uma combinação irresistível!',
+    '@JUAN',
+    [
+        '50ml de Tequila',
+        '30ml de Triple Sec',
+        '20ml de Suco de limão fresco',
+        'Sal para a borda do copo',
+       
+    ]
+));
+
+lista_categorias.push(new Categoria(
+    'Negroni',
+    'image/negroni.jpg',
+    'Um clássico italiano com gin, vermute e Campari.',
+    '@WILL',
+    [
+        '30ml de Gin',
+        '30ml de Vermute doce',
+        '30ml de Campari',
+        ,
+        'Casca de laranja para decorar'
+    ]
+));
+
+lista_categorias.push(new Categoria(
+    'Whisky',
+    'image/whisky.jpg',
+    'O tradicional whisky servido com gelo.',
+    '@MARIA',
+    [
+        '50ml de Whisky',
+        'Gelo'
+    ]
+));
+
+lista_categorias.push(new Categoria(
+    'Moscow Mule',
+    'image/moscow.jpg',
+    'Vodka, ginger beer e limão: uma mistura explosiva de sabores.',
+    '@MILENA',
+    [
+        '50ml de Vodka',
+        '100ml de Ginger Beer',
+        '10ml de Suco de limão',
+        ,
+        'Fatias de limão e hortelã para decorar'
+    ]
+));
 
 lista_categorias.forEach(categoria => {
     document.querySelector('#categoriaItems').innerHTML += `
-    
-    <div class="card mb-3" style="max-width: 540px; background-color: black;">
-    <div class="row g-0">
-        <div class="col-md-4">
-            <img src="${categoria.img}" class="img-fluid rounded-start" alt="...">
-        </div>
-        <div class="col-md-8">
-            <div class="card-body text-white"> <!-- Adicionando a classe text-white aqui -->
-                <h5 class="card-title">${categoria.titulo}</h5>
-                <p class="card-text">${categoria.desc}</p>
-                <p class="card-text">
-                    <small class="text-white">${categoria.marca}</small> <!-- Alterando a cor da marca para branco -->
-                </p>
+    <div class="card mb-3 " style="max-width: 540px; background-color: black; ">
+        <div class="row g-0 ">
+            <div class="col-md-4 ">
+                <img src="${categoria.img}" class="img-fluid rounded-start img-fluid rounded " alt="...">
+            </div>
+            <div class="col-md-8 ">
+                <div class="card-body text-white">
+                    <h5 class="card-title">${categoria.titulo}</h5>
+                    <p class="card-text">${categoria.desc}</p>
+                    <p class="card-text">
+                        <strong>Ingredientes:</strong><br>
+                        ${categoria.ingredientes.map(ingrediente => `<span>- ${ingrediente}</span><br>`).join('')}
+                    </p>
+                    <p class="card-text">
+                        <small class="text-white">${categoria.marca}</small>
+                    </p>
+                </div>
             </div>
         </div>
     </div>
-</div>
-
-
-    
-    `
+    `;
 });
