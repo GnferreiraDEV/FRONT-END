e8e08936e0657d3bb3f8faf7acf373406c0c7fb0
Gustavo Ferreira <Gustavo.nferreira@a.ucb.br>
2024-12-11 15:25:07 -0300

    UPDATE

--- bootstrap/script/Drinks.js
+++ bootstrap/script/Drinks.js
@@ -1,163 +1,85 @@
-lista_categorias = []
+// Lista para armazenar as categorias
+let lista_categorias = [];
 
+// Definição da classe Categoria
 class Categoria {
-    constructor(titulo, img, desc, marca, ingredientes){
-        this.titulo = titulo
-        this.img = img
-        this.desc = desc
-        this.marca = marca
-        this.ingredientes = ingredientes
+    constructor(titulo, img, desc) {
+        this.titulo = titulo;
+        this.img = img;
+        this.desc = desc;
     }
 }
 
+// Adicionando categorias à lista
 lista_categorias.push(new Categoria(
-    'Jack & Coke',
-    'image/preto.jpg',
-    'Uma combinação clássica de whisky e refrigerante, perfeita para qualquer ocasião.',
-    '@HELEN',
-    [
-        '50ml de Suco de limão',
-        '150ml de Coca-Cola',
-        'Gelo'
-    ]
+    'WHISKY',
+    '../image/CATALOGO/whisky.jpg',
+    'Uma combinação clássica de whisky e refrigerante, perfeita para qualquer ocasião.'
 ));
 
 lista_categorias.push(new Categoria(
     'Aperol Spritz',
-    'image/aperol.jpg',
-    'Um drink refrescante e leve, ideal para os dias quentes.',
-    '@FERREIRA',
-    [
-        '60ml de Aperol',
-        '90ml de Prosecco',
-        '30ml de Água com gás',
-        ,
-        'Fatias de laranja'
-    ]
+    '../image/CATALOGO/gin.jpg',
+    'Um drink refrescante e leve, ideal para os dias quentes.'
 ));
 
 lista_categorias.push(new Categoria(
     'Gin Tônica',
-    'image/gin.jpg',
-    'Refrescante e elegante, o gin tônica é um clássico.',
-    '@DUDA',
-    [
-        '50ml de Gin',
-        '150ml de Água tônica',
-        'Fatias de limão',
-        'Gelo'
-    ]
+    '../image/CATALOGO/rum.jpg',
+    'Refrescante e elegante, o gin tônica é um clássico.'
 ));
 
 lista_categorias.push(new Categoria(
     'Dry Martini',
-    'image/dry.jpg',
-    'Um dos drinks mais sofisticados e famosos do mundo.',
-    '@RARYSSON',
-    [
-        '60ml de Gin',
-        '10ml de Vermute seco',
-        'Azeitona ou casca de limão para decorar'
-    ]
+    '../image/CATALOGO/vinho.jpg',
+    'Um dos drinks mais sofisticados e famosos do mundo.'
 ));
 
 lista_categorias.push(new Categoria(
     'Marquerita',
-    'image/marguerita.jpg',
-    'Tequila, limão e sal: uma combinação irresistível!',
-    '@JUAN',
-    [
-        '50ml de Tequila',
-        '30ml de Triple Sec',
-        '20ml de Suco de limão fresco',
-        'Sal para a borda do copo',
-       
-    ]
+    '../image/CATALOGO/vodka.jpg',
+    'Tequila, limão e sal: uma combinação irresistível!'
 ));
 
 lista_categorias.push(new Categoria(
     'Negroni',
-    'image/negroni.jpg',
-    'Um clássico italiano com gin, vermute e Campari.',
-    '@WILL',
-    [
-        '30ml de Gin',
-        '30ml de Vermute doce',
-        '30ml de Campari',
-        ,
-        'Casca de laranja para decorar'
-    ]
+    '../image/CATALOGO/bebida.jpg',
+    'Um clássico italiano com gin, vermute e Campari.'
 ));
 
 lista_categorias.push(new Categoria(
     'Whisky',
-    'image/whisky.jpg',
-    'O tradicional whisky servido com gelo.',
-    '@MARIA',
-    [
-        '50ml de Whisky',
-        'Gelo'
-    ]
+    '../image/CATALOGO/whisky.jpg',
+    'O tradicional whisky servido com gelo.'
 ));
 
 lista_categorias.push(new Categoria(
     'Moscow Mule',
-    'image/moscow.jpg',
-    'Vodka, ginger beer e limão: uma mistura explosiva de sabores.',
-    '@MILENA',
-    [
-        '50ml de Vodka',
-        '100ml de Ginger Beer',
-        '10ml de Suco de limão',
-        ,
-        'Fatias de limão e hortelã'
-    ]
+    '../image/CATALOGO/whisky.jpg',
+    'Vodka, ginger beer e limão: uma mistura explosiva de sabores.'
 ));
 
 lista_categorias.push(new Categoria(
     'Caipirinha',
-    'image/caip.jpg',
-    'Cachaça e limão: uma mistura refrescante.',
-    '@STELLA',
-    [
-        '20ml de Limão',
-        '100ml de Cachaça',
-        'Gelo',
-        ,
-        'Fatias de limão e hortelã'
-    ]
+    '../image/CATALOGO/whisky.jpg',
+    'Cachaça e limão: uma mistura refrescante.'
 ));
 
 lista_categorias.push(new Categoria(
-    'Daikiri ',
-    'image/moran.jpg',
-    'Morango, Gin, combinação tropical.',
-    '@ROHOD',
-    [
-        '20ml de Extrato de morango',
-        '100ml de Gin',
-        'Gelo',
-        ,
-        'Fatias de Morango'
-    ]
+    'Daikiri',
+    '../image/CATALOGO/whisky.jpg',
+    'Morango, Gin, combinação tropical.'
 ));
 
+// Exibindo as categorias na página com o título acima da imagem e descrição abaixo
 lista_categorias.forEach(categoria => {
     document.querySelector('#categoriaItems').innerHTML += `
     <div class="card" style="width: 18rem; background-color: black;">
-        <img src="${categoria.img}" class="card-img-top" alt="${categoria.titulo}">
-        <div class="card-body">
-            <h5 class="card-title text-white">${categoria.titulo}</h5>
-            <p class="card-text text-white">${categoria.desc}</p>
-        </div>
-        <ul class="list-group list-group-flush">
-            ${categoria.ingredientes.map(ingrediente => `
-                <li class="list-group-item text-white">${ingrediente}</li>
-            `).join('')}
-        </ul>
-        <div class="card-body">
-            <a href="#" class="card-link text-white">${categoria.marca}</a>
-        </div>
+        <h5 class="card-title text-white" style="padding: 10px;">${categoria.titulo}</h5>
+        <img src="${categoria.img}" class="card-img-top" alt="${categoria.titulo}" style="height: 350px; width: 100%; object-fit: cover; border-radius: 10px;">
+        <p class="card-text text-white" style="padding: 10px;">${categoria.desc}</p>
     </div>
     `;
 });
+
+
