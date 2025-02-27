ae590e466f1caf19dc42e72e658967edb2548b55
Gustavo Ferreira <Gustavo.nferreira@a.ucb.br>
2025-02-27 17:12:49 -0300

    UPDATE

---
+++ introdução/LIVROS/index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Books</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+
+    <nav id="top">
+        <div class="container">
+            <h3> teste </h3>
+        </div>
+    </nav>
+
+    <header>
+        <div id="logo">
+
+            
+            <h2>The Bookworm's Den</h2>
+
+        </div>
+
+        <nav>
+
+            <a href="#">HOME</a>
+
+        </nav>
+
+    </header> 
+
+    <div class="categoria-bar">
+        <ul>
+            <li><a href="#">Livro</a></li>
+            <li><a href="#">Papelaria</a></li>
+            <li><a href="#">Presente</a></li>
+            <li><a href="#">Informática</a></li>
+            <li><a href="#">Jogos</a></li>
+            <li><a href="#">Oferta</a></li>
+            <li><a href="#">Promoções</a></li>
+            <li><a href="#">Pré-Venda</a></li>
+        </ul>
+    </div>
+
+    
+    <main>
+
+        
+
+        <h1>LIVROS</h1>
+
+        <h3>Harry Potter:</h3>
+
+        <section id="catalogo">
+
+           
+
+
+        </section>
+
+        <div class="card">
+            <img src="icons/image/livro7.jpg" alt="Capa do livro Fprtaleza digital" class="book-cover">
+            <div class="content">
+                <h2>Li e recomendo:</h2>
+                <p>
+                    Fiquei muito impactada com a leitura de <strong> Fortaleza Digital, o melhor e mais realista suspense tecnológico a chegar ao mercado em anos... Um susto a cada minuto." – The Midwest Book Review
+                </p>
+                <button class="btn">Eu quero!</button>
+            </div>
+        </div> 
+
+        <h1>FILMES</h1>
+
+        <h3>NACIONAIS:</h3>
+
+        <div id="filmes"></div> 
+
+       
+        
+
+    </main>
+
+    <footer>TODOS OS DIREITOS RESERVADOS</footer>
+
+    <script src="script.js"></script>
+
+    
+</body>
+</html>
