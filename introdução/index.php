ad6990638ec25a2e58880c6086ee746f8a1a8bed
Gustavo Ferreira <Gustavo.nferreira@a.ucb.br>
2025-02-27 16:52:06 -0300

    UPDATE

---
+++ introdução/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h1>hi girl, in ass nobody want to take</h1>
+
+<?php 
+
+    echo "Gustavo Ferreira <br>";
+    echo "Gustavo Ferreira <br>";
+    echo "Gustavo Ferreira <br>";
+
+    $palavra = "teste";
+    echo $palavra;
+
+?>
+
+
+    
+</body>
+</html>
